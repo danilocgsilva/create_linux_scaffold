8a3376e93ab39a76c78a3e666ab92e5ccbc2315d
Danilo Silva <danilocgsilva@gmail.com>
2018-06-14 07:13:18 -0300

    Now already can create the makefile too

--- shellutil.sh
+++ shellutil.sh
@@ -13,6 +13,18 @@ build_readme () {
   write_line "# $1" README.md
 }
 
+build_makefile () {
+  write_line "BIN ?= shellutil" Makefile
+  write_line "PREFIX ?= /usr/local" Makefile
+  write_line "" Makefile
+  write_line "install:" Makefile
+  write_line "  cp shellutil.sh \$(PREFIX)/bin/\$(BIN)" Makefile
+  write_line "  chmod +x \$(PREFIX)/bin/\$(BIN)" Makefile
+  write_line "" Makefile
+  write_line "uninstall:" Makefile
+  write_line "  rm -f \$(PREFIX)/bin/\$(BIN)" Makefile
+}
+
 ## Main function
 shellutil () {
     if [ -z $1 ]; then
@@ -23,6 +35,7 @@ shellutil () {
     mkdir $1
     cd $1
     build_readme $1
+    build_makefile $1
 }
 
 ## detect if being sourced and
